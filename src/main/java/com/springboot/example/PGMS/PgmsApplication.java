4119c66a26ef13c92923059c13db23a4a51c903c
Sharathcu <40916560+Sharathcu@users.noreply.github.com>
2018-09-14 14:42:08 +0530

    Add files via upload
    initial checkin

---
+++ src/main/java/com/springboot/example/PGMS/PgmsApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.example.PGMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgmsApplication.class, args);
+	}
+}
